d49efbaaadcce88c88dfe9690ebbac48177fdffa
Jordan Hayashi <jhhayashi@gmail.com>
2020-07-18 13:17:36 -0400

    Ensure meal name exists before creating new meal

--- components/NewMealDialog.js
+++ components/NewMealDialog.js
@@ -6,6 +6,7 @@ import Dialog from 'react-native-dialog'
 export default function NewMealDialog(props) {
   const [mealName, setMealName] = useState(undefined)
   const onSubmit = () => {
+    if (mealName === '') return
     props.onMealNameSet(mealName == undefined ? props.defaultMealName : mealName)
   }
   return (
@@ -14,7 +15,7 @@ export default function NewMealDialog(props) {
       <Dialog.Description>
         Enter a name for this meal
       </Dialog.Description>
-      <Dialog.Input value={mealName} defaultValue={props.defaultMealName} onChangeText={setMealName} />
+      <Dialog.Input clearButtonMode="always" value={mealName} defaultValue={props.defaultMealName} onChangeText={setMealName} />
       <Dialog.Button label="Cancel" onPress={props.onCancel} />
       <Dialog.Button label="OK" onPress={onSubmit} />
     </Dialog.Container>
